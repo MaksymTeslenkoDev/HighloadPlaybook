d865252972d511031fe0b1a6102c62ac4e1d5a24
maksim.t <maksim.teslenko.dev@gmail.com>
2025-03-28 11:06:50 +0200

    disable http, ws plugins to be global

--- domains/supply-chain/pkg/server/plugins/http.ts
+++ domains/supply-chain/pkg/server/plugins/http.ts
@@ -1,5 +1,5 @@
-import fp from 'fastify-plugin';
 import { Type } from '@sinclair/typebox';
+import { FastifyInstance } from 'fastify';
 
 enum APP_ERROR_CODES {
   SystemError = '000',
@@ -29,77 +29,78 @@ type ApiResponse = {
 
 export const ApiResponse = Type.Object({
   success: Type.Boolean(),
-  req_id: Type.String(),
+  req_id: Type.Optional(Type.String()),
   message: Type.Optional(Type.String()),
   code: Type.Optional(Type.Enum(APP_ERROR_CODES)),
   reason: Type.Optional(Type.Array(Type.String())),
 });
 
-export default fp(
-  async function http(fastify, { routes }: { routes: Record<string, any> }) {
-    fastify.setErrorHandler((err, req, reply) => {
-      if (reply.statusCode >= 500) {
-        req.log.error({ req, res: reply, err }, err?.message);
-        const error = new Error(
-          `Fatal error. Contact the support team with the id ${req.id}`,
-        );
-        reply.send(error);
-        return;
-      }
-      const { code, message, statusCode, validation } = err;
-
-      let errorPayload: ApiResponse = {
-        message: message,
-        code: code as APP_ERROR_CODES,
-        success: false,
-        req_id: req.id,
-      };
-      if (validation) {
-        errorPayload.reason =
-          validation.map((err) => `${err.keyword}: ${err.message}`) || [];
-        errorPayload.code = APP_ERROR_CODES.ValidationError;
-      }
-
-      req.log.info({ err }, err?.message);
-      reply.status(statusCode || 500).send(errorPayload);
-    });
-
-    for (const [iface, methods] of Object.entries(routes)) {
-      for (const [method, handler] of Object.entries(methods)) {
-        if (typeof handler !== 'function') {
-          continue;
-        }
-
-        fastify.route({
-          method: 'POST',
-          url: `/api/${iface}/${method}`,
-          schema: {
-            body: fastify.types[`api:${iface}:${method}:params`] || false,
-            response: {
-              200: Type.Object({
-                ...ApiResponse.properties,
-                data: fastify.types[`api:${iface}:${method}:returns`] || false,
-              }),
-            },
-          },
-          handler: async function (request) {
-            const { body } = request;
-            const handler = routes[iface][method];
-            if (!handler) {
-              throw new ServerError(
-                'Method not found',
-                APP_ERROR_CODES.NotFound,
-                404,
-              );
-            }
-            return await handler(body);
-          },
-        });
-      }
+export default async function http(
+  fastify: FastifyInstance,
+  { routes }: { routes: Record<string, any> },
+) {
+  fastify.setErrorHandler((err, req, reply) => {
+    if (reply.statusCode >= 500) {
+      req.log.error({ req, res: reply, err }, err?.message);
+      const error = new Error(
+        `Fatal error. Contact the support team with the id ${req.id}`,
+      );
+      reply.send(error);
+      return;
     }
-  },
-  {
-    name: 'http',
-    dependencies: ['api-schemas'],
-  },
-);
+    const { code, message, statusCode, validation } = err;
+
+    let errorPayload: ApiResponse = {
+      message: message,
+      code: code as APP_ERROR_CODES,
+      success: false,
+      req_id: req.id,
+    };
+    if (validation) {
+      errorPayload.reason =
+        validation.map((err) => `${err.keyword}: ${err.message}`) || [];
+      errorPayload.code = APP_ERROR_CODES.ValidationError;
+    }
+
+    req.log.info({ err }, err?.message);
+    reply.status(statusCode || 500).send(errorPayload);
+  });
+
+  for (const [iface, methods] of Object.entries(routes)) {
+    for (const [method, handler] of Object.entries(methods)) {
+      if (typeof handler !== 'function') {
+        continue;
+      }
+
+      fastify.route({
+        method: 'POST',
+        url: `/api/${iface}/${method}`,
+        schema: {
+          body: fastify.types[`api:${iface}:${method}:params`] || false,
+          response: {
+            200: Type.Object({
+              ...ApiResponse.properties,
+              data: fastify.types[`api:${iface}:${method}:returns`] || false,
+            }),
+          },
+        },
+        handler: async function (request) {
+          const { body } = request;
+          const handler = routes[iface][method];
+          if (!handler) {
+            throw new ServerError(
+              'Method not found',
+              APP_ERROR_CODES.NotFound,
+              404,
+            );
+          }
+          const response = await handler(body);
+          return {
+            success: true,
+            data: response,
+          };
+        },
+      });
+    }
+  }
+}
--- domains/supply-chain/pkg/server/plugins/ws.ts
+++ domains/supply-chain/pkg/server/plugins/ws.ts
@@ -1,48 +1,55 @@
-import fp from 'fastify-plugin';
 import websocket from '@fastify/websocket';
+import { FastifyInstance } from 'fastify';
 
-export default fp(
-  async function ws(fastify, { routes }: { routes: Record<string, any> }) {
-    await fastify.register(websocket);
-    await fastify.register(async (server) => {
-      server.get('/api', { websocket: true }, (socket, req) => {
-        socket.on('message', async (message: string) => {
-          try {
-            const { name, method, params } = JSON.parse(
-              message.toString(),
-            ) as app.WsRPCMessage;
-            const handler = routes[name][method];
+export default async function ws(
+  fastify: FastifyInstance,
+  { routes }: { routes: Record<string, any> },
+) {
+  await fastify.register(websocket, {
+    errorHandler(error, socket, request, reply) {
+      console.log('error ', error);
+      reply.status(404).send({ error: error.message });
+      socket.terminate();
+    },
+  });
 
-            if (!handler) {
-              return socket.send(JSON.stringify({ error: 'Method not found' }));
-            }
+  await fastify.register(async (server) => {
+    server.get('/api', { websocket: true }, (socket, req) => {
+      socket.on('message', async (message: string) => {
+        try {
+          const { name, method, params } = JSON.parse(
+            message.toString(),
+          ) as app.WsRPCMessage;
+          const handler = routes[name][method];
 
-            const paramsValidation =
-              fastify.typeValidators[`api:${name}:${method}:body`];
-            if (paramsValidation) {
-              if (!paramsValidation.Check(params)) {
-                return socket.send(
-                  JSON.stringify({
-                    error: 'Invalid parameters',
-                    details: [...paramsValidation.Errors(params)].map(
-                      (err) => `${err.message} \n`,
-                    ),
-                  }),
-                );
-              }
-            }
-
-            const result = await handler(params);
-            socket.send(JSON.stringify(result), { binary: false });
-          } catch (err) {
-            if (err instanceof Error) {
-              fastify.log.error(err.message);
-            }
-            socket.send('"Server error"', { binary: false });
+          if (!handler) {
+            return socket.send(JSON.stringify({ error: 'Method not found' }));
           }
-        });
+
+          const paramsValidation =
+            fastify.typeValidators[`api:${name}:${method}:body`];
+          if (paramsValidation) {
+            if (!paramsValidation.Check(params)) {
+              return socket.send(
+                JSON.stringify({
+                  error: 'Invalid parameters',
+                  details: [...paramsValidation.Errors(params)].map(
+                    (err) => `${err.message} \n`,
+                  ),
+                }),
+              );
+            }
+          }
+
+          const result = await handler(params);
+          socket.send(JSON.stringify(result), { binary: false });
+        } catch (err) {
+          if (err instanceof Error) {
+            fastify.log.error(err.message);
+          }
+          socket.send('"Server error"', { binary: false });
+        }
       });
     });
-  },
-  { name: 'websocket', dependencies: ['api-schemas'] },
-);
+  });
+}
