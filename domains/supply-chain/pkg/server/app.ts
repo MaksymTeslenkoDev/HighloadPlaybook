399569161ee62f7c3881b8647e2b8a00012a9142
maksim.t <maksim.teslenko.dev@gmail.com>
2025-03-30 22:21:52 +0300

    config tests

--- domains/supply-chain/pkg/server/app.ts
+++ domains/supply-chain/pkg/server/app.ts
@@ -1,88 +1,13 @@
-import { FastifyPluginAsync, FastifyServerOptions } from 'fastify';
+import { FastifyPluginAsync } from 'fastify';
 import { loadApplication } from './src/load';
-import { join } from 'node:path';
 import { configLoader } from './src/config';
 import Schemas from './plugins/api-schemas-loader';
 import WS from './plugins/ws';
 import http from './plugins/http';
+import serverOptions, { AppOptions } from './server.config';
 
-export interface AppOptions extends FastifyServerOptions {}
-// Pass --options via CLI arguments in command to enable these options.
-const options = (): AppOptions => {
-  const config = configLoader({ appPath: process.cwd() });
-  return {
-    disableRequestLogging: true,
-    requestIdLogLabel: 'reqId', // default
-    requestIdHeader: 'x-request-id',
-    genReqId: function (req) {
-      return (
-        (req.headers['x-server-request-id'] as string) || crypto.randomUUID()
-      );
-    },
-    logger: {
-      level: config.logger.logLevel || 'info',
-      transport: {
-        targets: [
-          {
-            target: 'pino/file',
-            options: {
-              destination: join(process.cwd(), config.logger.dir, 'app.log'),
-            },
-          },
-          {
-            target: 'pino/file',
-            options: { destination: 1 },
-          },
-        ],
-      },
-      timestamp: () => {
-        const dateString = new Date(Date.now()).toISOString();
-        return `,"@timestamp":"${dateString}"`;
-      },
-      redact: {
-        censor: '***',
-        paths: [
-          'req.headers.authorization',
-          'req.body.password',
-          'req.body.email',
-        ],
-      },
-      serializers: {
-        //@ts-ignore
-        req: function (request) {
-          const shouldLogBody = request.routeOptions.config.logBody === true;
-          return {
-            method: request.method,
-            url: request.raw.url,
-            routeUrl: request.routeOptions.url,
-            version: request.headers?.['accept-version'],
-            user: request.user?.id,
-            headers: {
-              host: request.headers['host'],
-              connection: request.headers['connection'],
-              test: request.headers[':path'],
-            },
-            body: shouldLogBody ? request.body : undefined,
-            hostname: request.hostname,
-            remoteAddress: request.ip,
-            remotePort: request.socket?.remotePort,
-          };
-        },
-        res: function (reply) {
-          return {
-            statusCode: reply.statusCode,
-            responseTime: reply.elapsedTime,
-          };
-        },
-      },
-    },
-    ajv: {
-      customOptions: {
-        removeAdditional: 'all',
-      },
-    },
-  };
-};
+const appConfig = configLoader({ appPath: process.cwd() });
+const options = serverOptions(appConfig);
 
 const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
@@ -100,6 +25,10 @@ const app: FastifyPluginAsync<AppOptions> = async (
 
   fastify.addHook('onSend', async function onSendRequestLogHook(req, res) {
     req.log.info({ req, res }, 'request completed 🎉');
+  });
+
+  fastify.get('/ping', {}, async () => {
+    return 'OK';
   });
 
   console.log(fastify.printPlugins());
